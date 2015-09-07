293d6af8a1fd318b9d17a7a12549962516fb7592
Eakkasit L. <ramgear@gmail.com>
2015-09-07 13:20:49 +0700

    Fix semihost

--- stm32f4-hal/_syscalls.c
+++ stm32f4-hal/_syscalls.c
@@ -8,79 +8,6 @@
 
 int errno;
 
-// ----------------------------------------------------------------------------
-
-#if !defined(OS_USE_SEMIHOSTING)
-
-#include <_ansi.h>
-#include <_syslist.h>
-#include <errno.h>
-//#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/times.h>
-#include <limits.h>
-#include <signal.h>
-
-void
-__initialize_args(int* p_argc, char*** p_argv);
-
-// This is the standard default implementation for the routine to
-// process args. It returns a single empty arg.
-// For semihosting applications, this is redefined to get the real
-// args from the debugger. You can also use it if you decide to keep
-// some args in a non-volatile memory.
-
-void __attribute__((weak))
-__initialize_args(int* p_argc, char*** p_argv)
-{
-  // By the time we reach this, the data and bss should have been initialised.
-
-  // The strings pointed to by the argv array shall be modifiable by the
-  // program, and retain their last-stored values between program startup
-  // and program termination. (static, no const)
-  static char name[] = "";
-
-  // The string pointed to by argv[0] represents the program name;
-  // argv[0][0] shall be the null character if the program name is not
-  // available from the host environment. argv[argc] shall be a null pointer.
-  // (static, no const)
-  static char* argv[2] =
-    { name, NULL };
-
-  *p_argc = 1;
-  *p_argv = &argv[0];
-  return;
-}
-
-// These functions are defined here to avoid linker errors in freestanding
-// applications. They might be called in some error cases from library
-// code.
-//
-// If you detect other functions to be needed, just let us know
-// and we'll add them.
-
-int
-raise(int sig __attribute__((unused)))
-{
-  errno = ENOSYS;
-  return -1;
-}
-
-int
-kill(pid_t pid, int sig);
-
-int
-kill(pid_t pid __attribute__((unused)), int sig __attribute__((unused)))
-{
-  errno = ENOSYS;
-  return -1;
-}
-
-#endif // !defined(OS_USE_SEMIHOSTING)
-
-// ----------------------------------------------------------------------------
-
 // If you need the empty definitions, remove the -ffreestanding option.
 
 #if __STDC_HOSTED__ == 1
@@ -89,7 +16,7 @@ char* __env[1] =
   { 0 };
 char** environ = __env;
 
-#if !defined(OS_USE_SEMIHOSTING)
+#if !defined(USE_SEMIHOSTING)
 
 // Forward declarations
 
@@ -309,7 +236,7 @@ _write(int file __attribute__((unused)), char* ptr __attribute__((unused)),
 
 // ----------------------------------------------------------------------------
 
-#else // defined(OS_USE_SEMIHOSTING)
+#else // defined(USE_SEMIHOSTING)
 
 // ----------------------------------------------------------------------------
 
@@ -334,7 +261,7 @@ _write(int file __attribute__((unused)), char* ptr __attribute__((unused)),
 #include <ctype.h>
 #include <signal.h>
 
-#include "arm/semihosting.h"
+#include "semihosting.h"
 
 int
 _kill (int pid, int sig);
@@ -1213,6 +1140,6 @@ getcwd (char *buf, size_t size)
   return buf;
 }
 
-#endif // defined OS_USE_SEMIHOSTING
+#endif // defined USE_SEMIHOSTING
 
 #endif // __STDC_HOSTED__ == 1
